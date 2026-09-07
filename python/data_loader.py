44b756cddb805fe2f450bc0146d339456924429a
Nitin salunke <nitinsalunke613@gmail.com>
2026-09-07 22:29:13 +0530

    Delete python directory

--- python/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from src.config import RAW_DATA_PATH
-
-
-def load_data():
-    df = pd.read_csv(RAW_DATA_PATH)
-    df.columns = df.columns.str.strip().str.replace(" ","_",regex=False)
-    return df
